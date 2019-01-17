591d96661eb983e1b5596ff7a9e3899016759a33
Elliott Parsons <elliottparsons13@gmail.com>
2019-01-17 19:04:20 +0000

    first commit

---
+++ js/Blockbuster.js
@@ -0,0 +1,30 @@
+let obj ={table:"Films", limit: 20};
+let dbParam = JSON.stringify(obj);
+let requestURL = "http://127.0.0.1:8080/api/film"
+let request = new XMLHttpRequest();
+request.open("GET",requestURL);
+request.setRequestHeader('Content-type','application/json');
+request.setRequestHeader('Access-Control-Allow-Origin','*');
+request.responseType="json"
+request.send();
+request.onload = function(){ 
+    let jsonString = request.response;
+    let txt="";
+    txt +="<table border='5'style='background-color:#aac4ef' table align='center' bordercolor='grey'>"
+    txt +="<th>Title</th>"
+    txt +="<th>Description</th>"
+    txt +="<th>Length</th>"
+    txt +="<th>Price</th>"
+    txt +="<th>Rating</th>"
+    for (x in jsonString){
+        txt += "<tr><td>" + jsonString[x].title + "</td>";
+        txt += "<td>" + jsonString[x].description + "</td>";
+        txt += "<td>" + jsonString[x].length + "</td>";
+        txt += "<td>" + jsonString[x].price + "</td>";
+        txt += "<td>" + jsonString[x].rating + "</td></tr>";
+    }
+    txt +="<table>"
+    console.log(jsonString[0].category);
+    let myH1=document.createElement('h1');
+    document.getElementById("Content").innerHTML = txt;
+}
